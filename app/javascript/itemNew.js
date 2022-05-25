5a61c26679939af5b6ec2b2fb08988e915393017
yukitaka7478 <yukitaka897@gmail.com>
2022-05-25 16:33:30 +0900

    マイページ機能の実装

--- app/javascript/itemNew.js
+++ app/javascript/itemNew.js
@@ -2,7 +2,7 @@ function salesTax (){
   const price = document.getElementById("item-price");
   const addTaxPrice =document.getElementById("add-tax-price");
   const profit = document.getElementById("profit");
-  price.addEventListener('input', () => {
+  price.addEventListener("input", () => {
     addTaxPrice.value = Math.floor(price.value * 0.1);
     addTaxPrice.innerHTML = `${addTaxPrice.value}`;
     profit.value = Math.floor(price.value - addTaxPrice.value);
